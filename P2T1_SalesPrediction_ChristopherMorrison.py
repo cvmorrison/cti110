ed5ad2eaa9b715434864b234f8670df185745617
cvmorrison <43639592+cvmorrison@users.noreply.github.com>
2019-04-10 02:55:19 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_ChristopherMorrison.py
@@ -0,0 +1,12 @@
+# Displays the annual profit
+# 04/09/2019
+# CTI-110 P2T1 - Sales Prediction
+# Christopher Morrison
+#
+
+
+total_sales = float(input("Enter the projected sales: "))
+
+profit = total_sales * 0.23
+
+print("The profit is $", format(profit, ",.2f"))
